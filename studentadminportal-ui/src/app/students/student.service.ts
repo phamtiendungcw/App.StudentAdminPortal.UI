769bd17b2e87846b2aacc904468e11ad7185f4c0
Phạm Tiến Dũng <56197293+phamtiendungcw@users.noreply.github.com>
2022-08-23 23:17:09 +0700

    Update an Existing Student with method Update in CRUD

--- studentadminportal-ui/src/app/students/student.service.ts
+++ studentadminportal-ui/src/app/students/student.service.ts
@@ -1,3 +1,4 @@
+import { UpdateStudentRequest } from './../models/api-models/update-student-request.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -19,5 +20,20 @@ export class StudentService {
     return this.httpClient.get<Student>(
       this.baseApiUrl + '/students/' + studentId
     );
+  }
+
+  updateStudent(studentId: string, studentRequest: Student): Observable<Student> {
+    const updateStudentRequest: UpdateStudentRequest = {
+      firstName: studentRequest.firstName,
+      lastName: studentRequest.lastName,
+      dateOfBirth: studentRequest.dateOfBirth,
+      email: studentRequest.email,
+      mobile: studentRequest.mobile,
+      genderId: studentRequest.genderId,
+      physicalAddress: studentRequest.address.physicalAddress,
+      postalAddress: studentRequest.address.postalAddress
+    }
+
+    return this.httpClient.put<Student>(this.baseApiUrl + '/students/' + studentId, updateStudentRequest);
   }
 }
--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -1,7 +1,10 @@
+import { GenderService } from './../../services/gender.service';
 import { Student } from './../../models/ui-models/student.model';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { StudentService } from '../student.service';
+import { Gender } from 'src/app/models/ui-models/gender.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-view-student',
@@ -11,6 +14,7 @@ import { StudentService } from '../student.service';
 export class ViewStudentComponent implements OnInit {
   studentId: string | null | undefined;
   private readonly newProperty = '';
+  genderList: Gender[] = [];
 
   student: Student = {
     id: '',
@@ -34,20 +38,38 @@ export class ViewStudentComponent implements OnInit {
 
   constructor(
     private readonly studentService: StudentService,
-    private readonly route: ActivatedRoute
-  ) {}
+    private readonly route: ActivatedRoute,
+    private readonly genderService: GenderService,
+    private snackbar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       this.studentId = params.get('id');
-
       if (this.studentId) {
-        this.studentService
-          .getStudent(this.studentId)
-          .subscribe((successResponse) => {
-            this.student = successResponse;
-          });
+        this.studentService.getStudent(this.studentId).subscribe((successResponse) => {
+          this.student = successResponse;
+        });
+        this.genderService.getGendersList().subscribe((successResponse) => {
+          this.genderList = successResponse;
+        });
       }
     });
+  }
+
+  onUpdate(): void {
+    // Call Student Service to Update Student
+    this.studentService.updateStudent(this.student.id, this.student)
+      .subscribe(
+        (successResponse) => {
+          // Show a notification
+          this.snackbar.open('Student update successfully', undefined, {
+            duration: 2000
+          });
+        },
+        (errorResponse) => {
+          // Log it
+        }
+      );
   }
 }
